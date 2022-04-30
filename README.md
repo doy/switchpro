4a7aa1317e77e6d7a5b5dc4a20b238d12efb148e
Jesse Luehrs <doy@tozt.net>
2022-04-30 18:45:24 -0400

    basic implementation

---
+++ README.md
@@ -0,0 +1,2 @@
+my switch pro controller doesn't appear to want to reconnect automatically if
+it gets disconnected, so here we are. run this program with the mac address of the controller, and press the pairing button once it's waiting.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use tokio_stream::StreamExt as _;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let controller = std::env::args().nth(1).unwrap().parse().unwrap();
+
+    let (_, session) = bluez_async::BluetoothSession::new().await?;
+    session.start_discovery().await?;
+
+    let mut stream = session.event_stream().await?;
+    while let Some(event) = stream.next().await {
+        if let bluez_async::BluetoothEvent::Device { id, .. } = &event {
+            let info = session.get_device_info(id).await?;
+            if info.mac_address == controller {
+                session.connect(id).await?;
+                break;
+            }
+        }
+    }
+
+    Ok(())
 }
